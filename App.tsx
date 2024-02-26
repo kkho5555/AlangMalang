8d5e46a107a5c213e9663fda1c582f6dc0f8ab1a
kkho5555 <kkho5555@gmail.com>
2024-02-26 18:55:43 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>알랑말랑 : 퀴즈모음.zip</Text>
+      <Text>개발중..</Text>
+      <Text>화이팅~~</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
